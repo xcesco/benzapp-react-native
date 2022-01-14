b490de99fe968c4a1a7a0638d0ecfbac11334189
xcesco <xcesco@gmail.com>
2022-01-15 00:45:38 +0100

    benzapp-react-native-7 aggancio ad FCM

--- src/screens/main-screen.tsx
+++ src/screens/main-screen.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 
 import {StackNavigationProp} from '@react-navigation/stack';
 import {useNavigation} from '@react-navigation/native';
 import {RootStackParamList} from '../navigation/root-stack-param-list';
 import AppMainContent from '../components/app-main-content';
-import {AppPage} from '../components/app-page';
-import {Button, Text} from 'react-native-paper';
 
 type screenProp = StackNavigationProp<RootStackParamList, 'Main'>;
 
@@ -20,34 +18,35 @@ export function MainScreen() {
     });
   };
 
-  return (
-          <View>
-            <AppMainContent/>
-            <AppPage title="ciao">
-              <View style={{width: '80%', alignItems: 'center', alignContent: 'center'}}>
-                <Text>Home Screen</Text>
-                <Button mode="contained" onPress={navigateToLogin}>
-                  <Text>Goto login</Text>
-                </Button>
-                <Button mode="contained" onPress={() => navigation.navigate('VehicleList')}>
-                  <Text>Goto VehicleList</Text>
-                </Button>
-                <Button mode="contained" onPress={() => navigation.navigate('VehicleDetail')}>
-                  <Text>Goto VehicleDetail</Text>
-                </Button>
-                <Button mode="contained" onPress={() => navigation.navigate('VehicleQRCodeDetail')}>
-                  <Text>Goto VehicleQRCodeDetail</Text>
-                </Button>
-                <Button mode="contained" onPress={() => navigation.navigate('RefuelingList')}>
-                  <Text>Goto RefuelingList</Text>
-                </Button>
-                <Button mode="contained" onPress={() => navigation.navigate('RefuelingDetail')}>
-                  <Text>Goto RefuelingDetail</Text>
-                </Button>
-              </View>
-            </AppPage>
-          </View>
-  );
+  /**
+   {/*<AppPage title="ciao">*/
+
+// {/*  <View style={{width: '80%', alignItems: 'center', alignContent: 'center'}}>*/}
+// {/*    <Text>Home Screen</Text>*/}
+// {/*    <Button mode="contained" onPress={navigateToLogin}>*/}
+// {/*      <Text>Goto login</Text>*/}
+// {/*    </Button>*/}
+// {/*    <Button mode="contained" onPress={() => navigation.navigate('VehicleList')}>*/}
+// {/*      <Text>Goto VehicleList</Text>*/}
+// {/*    </Button>*/}
+// {/*    <Button mode="contained" onPress={() => navigation.navigate('VehicleDetail')}>*/}
+// {/*      <Text>Goto VehicleDetail</Text>*/}
+// {/*    </Button>*/}
+// {/*    <Button mode="contained" onPress={() => navigation.navigate('VehicleQRCodeDetail')}>*/}
+// {/*      <Text>Goto VehicleQRCodeDetail</Text>*/}
+// {/*    </Button>*/}
+// {/*    <Button mode="contained" onPress={() => navigation.navigate('RefuelingList')}>*/}
+// {/*      <Text>Goto RefuelingList</Text>*/}
+// {/*    </Button>*/}
+// {/*    <Button mode="contained" onPress={() => navigation.navigate('RefuelingDetail')}>*/}
+// {/*      <Text>Goto RefuelingDetail</Text>*/}
+// {/*    </Button>*/}
+// {/*  </View>*/}
+// {/*</AppPage>*/}
+// {/*</View>*/}
+
+
+  return (<AppMainContent/>);
 }
 
 const style = StyleSheet.create({
