9c6b645eb6532677e37893cdac57af7dc067e064
xcesco <xcesco@gmail.com>
2022-01-15 18:27:39 +0100

    benzapp-react-native-14 main > elenco distributori

--- src/ui/stations/station-list-fragment.tsx
+++ src/ui/stations/station-list-fragment.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Text} from 'react-native-paper';
+import {FlatList, Image, StyleSheet, View} from 'react-native';
+import {Colors, Text} from 'react-native-paper';
 import {Station} from '../../repositories/model/station';
+import {getMarchioImage} from '../../repositories/model/marchio';
 
 export default function StationListFragment(props: { componentId?: string; list: Station[] }) {
-  return (<View>
-    <Text>station list fragmetn: {props.list.length}</Text>
+  // @ts-ignore
+  const _renderItem = (renderItem: { item: Station }) => {
+    return (
+            <View>
+              <Image style={{width:48, height:48}} source={getMarchioImage(renderItem.item.marchio)}/>
+              <Text style={style.item}>{renderItem.item.marchio}</Text>
+            </View>
+    );
+  }
+
+  return (<View style={style.container}>
+    <FlatList data={props.list} renderItem={_renderItem}/>
   </View>);
 };
 
 const style = StyleSheet.create({
   container: {
+    backgroundColor: Colors.white,
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
